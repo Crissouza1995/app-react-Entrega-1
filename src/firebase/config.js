151a7c7ab73a5950e40ba202579cfceb13853d5a
Crissouza1995 <cristianfabiansouza@gmail.com>
2023-08-23 13:40:59 -0300

    deploy website2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,11 +5,11 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apikey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
+  apiKey: process.env.REACT_APP_apiKey ,
+  authDomain: process.env.REACT_APP_authDomain ,
+  projectId: process.env.REACT_APP_projectId ,
   storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
   appId: process.env.REACT_APP_appId
 };
 
